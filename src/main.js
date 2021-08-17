ff58a07c0e296ed78e1a82c94f8d593b1984e179
damncorwell <64529786+damncorwell@users.noreply.github.com>
2021-08-17 17:55:31 +0300

    Подключил нужные библиотеки

--- src/main.js
+++ src/main.js
@@ -1,10 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import store from './store'
+import BootstrapVue from "bootstrap-vue";
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import Paginate from 'vuejs-paginate'
 
 Vue.config.productionTip = false
 
+Vue.component('paginate', Paginate)
+
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app')
+
+Vue.use(BootstrapVue)
